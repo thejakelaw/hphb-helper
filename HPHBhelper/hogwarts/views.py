ddb3699c35f528f42cdb0bc2ea1a75e503a0e34f
Jake Law <jakelaw@me.com>
2020-10-30 21:58:15 -0500

    active games are displayed

--- HPHBhelper/hogwarts/views.py
+++ HPHBhelper/hogwarts/views.py
@@ -1,25 +1,34 @@
+import random
+from string import ascii_uppercase, digits
+
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
 from .forms import GameForm
-import random
+from .models import Games
 
 
 def lobby(request):
     # Create Game
-
     # Join Game
-    # select from list
     return HttpResponse("Yer not a wizard yet, 'Arry! Welcome to the Lobby. Check back soon.")
-
 
 def create_game(request):
     letters = 'ABCDEFGHJKLMNPQRSTUVWXYZ1234567890'
-    shortcode = ''.join((random.choice(letters) for i in range(4)))
-    form = GameForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        messages.success(request, "Troll -- in the dungeons -- thought you ought to know.")
-        return redirect('hogwarts:HPHBhelper')
-    return render(request, 'HPHBhelper/index.html', {'form': form, 'shortcode': shortcode})
+    # letters = ascii_uppercase + digits
+    active_games = Games.objects.filter(isActive=True)
+    shortcode = ''.join(random.sample(letters, 4))
 
+    if "shortcode" in request.POST:
+        shortcode = request.POST["shortcode"]
+        game, created = Games.objects.get_or_create(shortcode=shortcode)
+
+        msg = "game created" if created else "game already there"
+
+        # "Troll -- in the dungeons -- thought you ought to know."
+        messages.success(request, msg)
+        return redirect('create')
+
+    return render(request, 'HPHBhelper/index.html',
+                  {'shortcode': shortcode, 'active_games': active_games})
