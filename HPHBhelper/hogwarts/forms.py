3d2ec4ae683ea7c940b420e2eb5fce33efecaa48
Jake Law <jakelaw@me.com>
2020-11-18 10:32:02 -0600

    for code check in

--- HPHBhelper/hogwarts/forms.py
+++ HPHBhelper/hogwarts/forms.py
@@ -2,7 +2,8 @@ from django.forms import ModelForm
 
 from .models import Players
 
+
 class PlayerForm(ModelForm):
     class Meta:
         model = Players
-        fields = ['name', 'wizard']
+        fields = ['name', 'wizard', 'player_img']
